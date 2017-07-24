0bc5c1ccac1cfd724e0fc026533087d7e3b9bbb6
feisha10 <feisha10@qq.com>
2017-07-24 23:54:09 +0800

    config handler

---
+++ client/game/Assets/Scripts/Manager/ConfigManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ConfigManager:Singleton<ConfigManager> {
+
+	private Dictionary<string, Dictionary<int, ConfigBase>> _allConfigs = new Dictionary<string, Dictionary<int, ConfigBase>>();
+
+    private string GetUrl(Type type)
+    {
+        return type.Name.Substring(6);
+    }
+    public T GetConfig<T>(int id) where T : ConfigBase, new()
+    {
+		string url = GetUrl(typeof(T));
+		if(string.IsNullOrEmpty(url)==false)
+		{
+
+		}
+		return null;
+	}
+
+}
---
+++ client/game/Assets/Scripts/Manager/ResourcesManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourcesManager:Singleton<ResourcesManager> {
+
+	public Object LoadAsset(string path)
+	{
+		return Resources.Load(path);
+	}
+}
